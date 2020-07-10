490df0d58c71add13b935f9959876f38a567d1e9
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-07-10 19:17:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,10 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
+def introduction_with_language 
+puts name 
+puts language
+
+puts " HI my name is #{name} and i am learning to proggramn in #{language}"
+end 
